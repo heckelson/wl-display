e647222b18ad8611e3087388265f0196f338b61a
heckelson <26191361+heckelson@users.noreply.github.com>
2026-02-01 17:02:55 +0100

    can now print touch coordinates

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,9 @@ int x, y, z;
 
 uint32_t draw_buffer[DRAW_BUF_SIZE / 4];
 
+// callback functions
 static uint32_t tick_cb_func();
+void touchscreen_read_cb_func(lv_indev_t *, lv_indev_data_t *);
 
 void setup() {
   Serial.begin(115200);
@@ -50,6 +52,10 @@ void setup() {
   lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x003a57),
                             LV_PART_MAIN);
 
+  lv_indev_t *indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+  lv_indev_set_read_cb(indev, touchscreen_read_cb_func);
+
   lv_obj_t *label = lv_label_create(lv_screen_active());
   lv_label_set_text(label, "Hello PlatformIO, I'm LVGL!");
   lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff),
@@ -66,4 +72,32 @@ void loop() {
 static uint32_t tick_cb_func() {
   // easy as pie.
   return millis();
+}
+
+void touchscreen_read_cb_func(lv_indev_t *indev, lv_indev_data_t *data) {
+  if (touchscreen.tirqTouched() && touchscreen.touched()) {
+    TS_Point p = touchscreen.getPoint();
+    // Calibrate Touchscreen points with map function to the correct width and
+    // height
+    x = map(p.x, 200, 3700, 1, SCREEN_WIDTH);
+    y = map(p.y, 240, 3800, 1, SCREEN_HEIGHT);
+    z = p.z;
+
+    data->state = LV_INDEV_STATE_PRESSED;
+
+    data->point.x = x;
+    data->point.y = y;
+
+    // Print Touchscreen info about X, Y and Pressure (Z) on the Serial Monitor
+    Serial.print("X = ");
+    Serial.print(x);
+    Serial.print(" | Y = ");
+    Serial.print(y);
+    Serial.print(" | Pressure = ");
+    Serial.print(z);
+    Serial.println();
+
+  } else {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
 }
